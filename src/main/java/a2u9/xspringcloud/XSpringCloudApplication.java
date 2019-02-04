128df65f072db7e4c82b5c8f8cc23386e3e89174
antonvm <a@r.ru>
2019-02-04 22:04:13 +0300

    init Spring Boot application

---
+++ src/main/java/a2u9/xspringcloud/XSpringCloudApplication.java
@@ -0,0 +1,14 @@
+package a2u9.xspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XSpringCloudApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/a2u9/xspringcloud/XSpringCloudApplicationTests.java
@@ -0,0 +1,17 @@
+package a2u9.xspringcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XSpringCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
